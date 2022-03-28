a4e399cad9cb09fc0466dacfd64ecfdc97e87af2
Da-Jin Chu <dajinchu@gmail.com>
2022-03-28 02:47:38 -0400

    vercel

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-amazon-linux-extras install rust1
+# amazon-linux-extras install rust1
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh 
 wasm-pack build ./rust
 cd frontend && npm run build
